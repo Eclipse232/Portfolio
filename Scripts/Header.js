7aaaa1728e1e8de1e368001dd7e209a59a0b27ad
Nick Ketelaars <nickketelaars@outlook.com>
2023-03-07 10:22:19 +0100

    Header bug fix

--- Scripts/Header.js
+++ Scripts/Header.js
@@ -9,11 +9,11 @@ const name = "Nick Ketelaars";
 const span = document.querySelector("h1 span");
 
 let i = 0;
-setInterval(() => {
+const typingInterval = setInterval(() => {
 	span.textContent += name[i];
 	i++;
 
 	if (i === name.length) {
-		clearInterval();
+		clearInterval(typingInterval);
 	}
 }, 150);
